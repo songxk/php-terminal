000e29dd5e83bb5a19833011a36b5063bf5f2e43
songxk <songxinkai@gmail.com>
2016-01-14 12:16:06 +0800

    commit code

---
+++ Color.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @title: Color.php
+ * @description: PHP终端输出彩色
+ * @author: songxinkai@gmail.com
+ * @date: 2016-01-14
+ * @copyright  Copyright (c) 2015-2016 www.songxinkai.com
+ */
+class Color {
+    const BLACK         = '30';
+    const RED           = '31';
+    const GREEN         = '32';
+    const YELLOW        = '33';
+    const BLUE          = '34';
+    const MAGENTA       = '35';
+    const CYAN          = '36';
+    const LIGHT_GRAY    = '37';
+    const DARK_GRAY     = '90';
+    const LIGHT_RED     = '91';
+    const LIGHT_GREEN   = '92';
+    const LIGHT_YELLOW  = '93';
+    const LIGHT_BLUE    = '94';
+    const LIGHT_MAGENTA = '95';
+    const LIGHT_CYAN    = '96';
+    const WHITE         = '97';
+
+    const BOLD       = '1';
+    const DIM        = '2';
+    const UNDERLINED = '4';
+    const REVERSE    = '7';
+    const HIDDEN     = '8';
+
+    public static function format($string, $color, $style = array()) {
+        $result = "\033[";
+        $result .= implode(';', array_merge($style, array($color)));
+        return $result.'m'.$string."\033[0m";
+    }
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+# php-terminal-color
+You can print colorful text in terminal with this library.
+
+## Install
+
+get it by
+
+`git clone https://github.com/songxk/php-terminal-color.git`
+
+then include it into your PHP script
+
+`require 'Color.php'`
+
+## Example
+
+``` php
+<?php
+$style = array(
+    Color::BOLD,
+    Color::UNDERLINED,
+);
+echo 'Enjoy ' . Color::format('php-terminal-color', Color::LIGHT_MAGENTA, $style),' library';'''')'')
+```
